fe29ab6caa7cc41150d7d21d76fe311d2db8efc1
dashdemir94 <75512880+dashdemir94@users.noreply.github.com>
2021-05-16 20:59:36 +0300

    Add files via upload

--- library/catalogue_of_books/models.py
+++ library/catalogue_of_books/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+    
+class My_Library(models.Model):
+    Books = models.CharField(max_length=200, verbose_name= 'Книга')
+    Author = models.CharField(max_length=200, verbose_name= 'Автор')
+    Genre = models.CharField(max_length=200, verbose_name= 'Жанр')
+    data = models.DateField( verbose_name= 'Дата')
+    coments = models.CharField(max_length=300, verbose_name= 'Коментарии')
 
-class Book(models.Model):
-	title = models.CharField(max_length=200)
-	date = models.DateTimeField()
-	price = models.IntegerField()
-	authors = models.CharField(max_length=200)
-	comments = models.CharField(max_length=200, default='SOME STRING')
+    def __str__(self):
+        return self.Books
 
-	def publish(self):
-        	self.published_date = timezone.now()
-        	self.save()
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+        
 
-	class Meta1:
-		verbose_name = 'Книги'
 
+    
+   
+    
 
